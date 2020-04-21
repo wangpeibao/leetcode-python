d32bcaa14f3f0bc6760eac18af2052ed8ea9dc2b
wangpeibao <wangpeibao@troila.com>
2020-04-21 10:39:02 +0800

    简单题库-1

---
+++ easy/easy1.py
@@ -0,0 +1,36 @@
+'''
+1. 两数之和
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+'''
+
+
+class Solution:
+    def twoSum(self, nums, target: int):
+        # 将nums转化成value:index的dict, 再遍历数组查找target - value的索引
+        num_dict = dict()
+        for index, value in enumerate(nums):
+            num_dict[value] = index
+        for index, value in enumerate(nums):
+            if target - value in num_dict.keys():
+                if index != num_dict[target - value]:  # 注意同一个元素不会被重复利用
+                    return [index, num_dict[target - value]]
+        return []
+
+
+so = Solution()
+
+examples = [
+    {"nums": [2, 7, 11, 15], "target": 9, "result": [0, 1]},
+    {"nums": [3, 2, 4], "target": 6, "result": [1, 2]}
+]
+
+for exa in examples:
+    result = so.twoSum(exa["nums"], exa["target"])
+    print(result == exa["result"])
