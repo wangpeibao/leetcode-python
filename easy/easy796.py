9c01ead9be30b9928b4fef1e8bfef666f921689a
wangpeibao <wangpeibao@troila.com>
2020-05-28 10:11:47 +0800

    2020-05-28

---
+++ easy/easy796.py
@@ -0,0 +1,28 @@
+'''
+796. 旋转字符串
+给定两个字符串, A 和 B。
+A 的旋转操作就是将 A 最左边的字符移动到最右边。 例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea' 。
+如果在若干次旋转操作之后，A 能变成B，那么返回True。
+示例 1:
+输入: A = 'abcde', B = 'cdeab'
+输出: true
+示例 2:
+输入: A = 'abcde', B = 'abced'
+输出: false
+注意：
+A 和 B 长度不超过 100。
+通过次数9,860提交次数19,695
+'''
+
+
+class Solution:
+    def rotateString(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+        return True if A in B * 2 else False
+
+
+so = Solution()
+
+print(so.rotateString("abcde", "cdeab") == True)
+print(so.rotateString("abcde", "abced") == False)
---
+++ easy/easy806.py
@@ -0,0 +1,52 @@
+'''
+806. 写字符串需要的行数
+我们要把给定的字符串 S 从左到右写到每一行上，每一行的最大宽度为100个单位，如果我们在写某个字母的时候会使这行超过了100 个单位，
+那么我们应该把这个字母写到下一行。我们给定了一个数组 widths ，这个数组 widths[0] 代表 'a' 需要的单位， widths[1] 代表 'b' 需要的单位，...， widths[25] 代表 'z' 需要的单位。
+
+现在回答两个问题：至少多少行能放下S，以及最后一行使用的宽度是多少个单位？将你的答案作为长度为2的整数列表返回。
+示例 1:
+输入:
+widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = "abcdefghijklmnopqrstuvwxyz"
+输出: [3, 60]
+解释:
+所有的字符拥有相同的占用单位10。所以书写所有的26个字母，
+我们需要2个整行和占用60个单位的一行。
+示例 2:
+输入:
+widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = "bbbcccdddaaa"
+输出: [2, 4]
+解释:
+除去字母'a'所有的字符都是相同的单位10，并且字符串 "bbbcccdddaa" 将会覆盖 9 * 10 + 2 * 4 = 98 个单位.
+最后一个字母 'a' 将会被写到第二行，因为第一行只剩下2个单位了。
+所以，这个答案是2行，第二行有4个单位宽度。
+注:
+字符串 S 的长度在 [1, 1000] 的范围。
+S 只包含小写字母。
+widths 是长度为 26的数组。
+widths[i] 值的范围在 [2, 10]。
+'''
+
+from typing import List
+
+class Solution:
+    def numberOfLines(self, widths: List[int], S: str) -> List[int]:
+        each_count = 0
+        count = 0
+        index = 0
+        length = len(S)
+        while index < length:
+            if each_count + widths[ord(S[index]) - ord("a")] > 100:
+                each_count = 0
+                count += 1
+            else:
+                each_count += widths[ord(S[index]) - ord("a")]
+                index += 1
+        return [count + 1, each_count]
+
+
+so = Solution()
+
+print(so.numberOfLines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "abcdefghijklmnopqrstuvwxyz") == [3, 60])
+print(so.numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "bbbcccdddaaa") == [2, 4])
