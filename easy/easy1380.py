75b82b53de2189e6af258ad51aa345f17b7c3bea
wangpb1 <wangpb1@sugon.com>
2020-10-29 20:59:52 +0800

    easy1370, 74, 80

---
+++ easy/easy1380.py
@@ -0,0 +1,53 @@
+"""
+1380. 矩阵中的幸运数
+给你一个 m * n 的矩阵，矩阵中的数字 各不相同 。请你按 任意 顺序返回矩阵中的所有幸运数。
+幸运数是指矩阵中满足同时下列两个条件的元素：
+在同一行的所有元素中最小
+在同一列的所有元素中最大
+
+示例 1：
+输入：matrix = [[3,7,8],[9,11,13],[15,16,17]]
+输出：[15]
+解释：15 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
+示例 2：
+输入：matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+输出：[12]
+解释：12 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
+示例 3：
+输入：matrix = [[7,8],[1,2]]
+输出：[7]
+
+提示：
+m == mat.length
+n == mat[i].length
+1 <= n, m <= 50
+1 <= matrix[i][j] <= 10^5
+矩阵中的所有元素都是不同的
+"""
+from typing import List
+
+
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        row_len = len(matrix)
+        result = []
+        # 获取每行中的最小值
+        for row in matrix:
+            min_value = 10 ** 5 + 1
+            min_index = 0
+            for index, value in enumerate(row):
+                if value < min_value:
+                    min_index = index
+                    min_value = value
+            status = True
+            for x in range(row_len):
+                if min_value < matrix[x][min_index]:
+                    status = False
+            if status:
+                result.append(min_value)
+        return result
+
+so = Solution()
+print(so.luckyNumbers(matrix = [[3,7,8],[9,11,13],[15,16,17]]) == [15])
+print(so.luckyNumbers(matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]) == [12])
+print(so.luckyNumbers(matrix = [[7,8],[1,2]]) == [7])
