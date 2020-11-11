2aa7f4ae71160a2138b42ccd871ec3a3ffbf6b85
wangpb1 <wangpb1@sugon.com>
2020-11-11 09:07:16 +0800

    easy1566.72.76

---
+++ easy/easy1572.py
@@ -0,0 +1,49 @@
+"""
+1572. 矩阵对角线元素的和
+给你一个正方形矩阵 mat，请你返回矩阵对角线元素的和。
+请你返回在矩阵主对角线上的元素和副对角线上且不在主对角线上元素的和。
+示例  1：
+输入：mat = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+输出：25
+解释：对角线的和为：1 + 5 + 9 + 3 + 7 = 25
+请注意，元素 mat[1][1] = 5 只会被计算一次。
+示例  2：
+输入：mat = [[1,1,1,1],
+            [1,1,1,1],
+            [1,1,1,1],
+            [1,1,1,1]]
+输出：8
+示例 3：
+输入：mat = [[5]]
+输出：5
+提示：
+n == mat.length == mat[i].length
+1 <= n <= 100
+1 <= mat[i][j] <= 100
+"""
+from typing import List
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        length = len(mat)
+        count = 0
+        for index in range(length):
+            count += mat[index][index]
+            count += mat[index][length - index - 1]
+        if length % 2 == 1:
+            count -= mat[length // 2][length // 2]
+        return count
+
+
+so = Solution()
+print(so.diagonalSum(mat=[[1, 2, 3],
+                          [4, 5, 6],
+                          [7, 8, 9]]) == 25)
+print(so.diagonalSum(mat=[[1, 1, 1, 1],
+                          [1, 1, 1, 1],
+                          [1, 1, 1, 1],
+                          [1, 1, 1, 1]]) == 8)
+print(so.diagonalSum(mat=[[5]]) == 5)
